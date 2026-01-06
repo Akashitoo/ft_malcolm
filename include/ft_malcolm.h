ceface62c9db41cc10c9d8ea943f201eb338a4cb
Akash <akash04@hotmail.fr>
2026-01-06 13:56:11 +0100

    test

---
+++ include/ft_malcolm.h
@@ -0,0 +1,9 @@
+#ifndef FT_MALCOLM_H
+# define FT_MALCOLM_H
+
+# include <sys/socket.h>
+# include <net/ethernet.h>
+# include <signal.h> 
+# include <stdio.h>
+# include <arpa/inet.h>
+#endif
--- src/main.c
+++ src/main.c
@@ -1,9 +1,33 @@
 #include "ft_malcolm.h"
 
+int g_stop = 0;
+
+void handler(int sig)
+{
+	printf("\nSignal reçu:%d\n", sig);
+	g_stop = 1;
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 4)
+	signal(SIGINT, handler);
+
+	unsigned char buffer[1518];
+
+	if (argc != 5)
 		return 1;
-	
+	int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if (sock < 0)
+		printf("erreur\n");
+	while (!g_stop)
+	{
+		int b = recvfrom(sock, buffer, 1518, 0, NULL, NULL);
+		if (b > 0)
+		{
+			printf("trame recu \n");
+			return 1;
+		}
+	}
+
 	(void)argv;
 }
